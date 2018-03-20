09efed36a6d46201300c5bc7f751314a25ed17f8
blizzard190 <22256@ma-web.nl>
2018-03-20 14:44:31 +0100

    Added
    Boss compatible

--- Assets/Scripts/Camera/Camera_Follow.cs
+++ Assets/Scripts/Camera/Camera_Follow.cs
@@ -5,26 +5,79 @@ using UnityEngine;
 public class Camera_Follow : MonoBehaviour
 {
     [SerializeField]
-    private GameObject player;
-    private Vector3 offset;
+    private GameObject _player;
+    private GameObject _boss;
+    private Vector3 _offset;
     [SerializeField]
-    private Vector3 extra;
-    private bool reach = false;
-    private bool low = false;
-    private bool bossCamera = false;
-    private Camera mainCamera;
+    private Vector3 _extra;
+    private bool _reach = false;
+    private bool _low = false;
+    private bool _bossCamera = false;
+    private Camera _mainCamera;
     public float shakeTimer;
     public float shakeAmout;
 
     void Start()
     {
-        mainCamera = gameObject.GetComponent<Camera>();
-        offset = transform.position - player.transform.position + extra;
+        _boss = GameObject.FindWithTag("Boss");
+        _mainCamera = gameObject.GetComponent<Camera>();
+        _offset = transform.position - _player.transform.position + _extra;
     }
 
     void Update()
     {
         Follow();
+        ShakeTimer();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Max_Height")
+        {
+            _reach = true;
+        }
+        if (other.gameObject.tag == "Min_Height")
+        {
+            _low = true;
+        }
+        if(other.gameObject.tag == "BossRoom")
+        {
+            _bossCamera = true;
+        }
+    }
+
+    void Follow()
+    {
+        if (!_reach && !_bossCamera)
+        {
+            this.transform.position = _player.transform.position + _offset;
+        }
+        else if (_reach && !_bossCamera)
+        {
+            this.transform.position = new Vector3(_player.transform.position.x + _extra.x, this.transform.position.y, this.transform.position.z);
+            if (this.transform.position.y > _player.transform.position.y)
+            {
+                _reach = false;
+            }
+        }
+        else if (_low && !_bossCamera)
+        {
+            this.transform.position = new Vector3(_player.transform.position.x + _extra.x, this.transform.position.y, this.transform.position.z);
+            if(this.transform.position.y < _player.transform.position.y)
+            {
+                _low = false;
+            }
+        }
+        if (_bossCamera)
+        {
+            this.transform.position = new Vector3(_boss.transform.position.x, this.transform.position.y, this.transform.position.z);
+            _mainCamera.enabled = true;
+            _mainCamera.orthographicSize = Mathf.Lerp(_mainCamera.orthographicSize, 10f, 0.2f);
+        }
+    }
+
+    public void ShakeTimer()
+    {
         if (shakeTimer >= 0)
         {
             Vector2 shakePos = Random.insideUnitCircle * shakeAmout;
@@ -33,50 +86,6 @@ public class Camera_Follow : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.tag == "Max_Height")
-        {
-            reach = true;
-        }
-        if (other.gameObject.tag == "Min_Height")
-        {
-            low = true;
-        }
-        if(other.gameObject.tag == "BossRoom")
-        {
-            bossCamera = true;
-        }
-    }
-
-void Follow()
-    {
-        if (!reach && !bossCamera)
-        {
-            this.transform.position = player.transform.position + offset;
-        }
-        else if (reach && !bossCamera)
-        {
-            this.transform.position = new Vector3(player.transform.position.x + extra.x, this.transform.position.y, this.transform.position.z);
-            if (this.transform.position.y > player.transform.position.y)
-            {
-                reach = false;
-            }
-        }
-        else if (low && !bossCamera)
-        {
-            this.transform.position = new Vector3(player.transform.position.x + extra.x, this.transform.position.y, this.transform.position.z);
-            if(this.transform.position.y < player.transform.position.y)
-            {
-                low = false;
-            }
-        }
-        if (bossCamera)
-        {
-            mainCamera.enabled = true;
-            mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, 10f, 0.2f);
-        }
-    }
     public void ShakeCamera(float shakePwr, float shakeDur)
     {
         shakeAmout = shakePwr;
