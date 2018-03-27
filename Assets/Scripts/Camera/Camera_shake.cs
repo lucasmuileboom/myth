45eeff961ec9771f736f3489d30207266b5a866e
blizzard190 <22256@ma-web.nl>
2018-03-27 09:57:18 +0200

    Camera Script
    Appart script for de camera shake

---
+++ Assets/Scripts/Camera/Camera_shake.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_shake : MonoBehaviour {
+
+    public float shakeTimer;
+    public float shakeAmout;
+
+    private void Update()
+    {
+        ShakeTimer();
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ShakeCamera(1,0.2f);
+        }
+    }
+
+    public void ShakeTimer()
+    {
+        if (shakeTimer >= 0)
+        {
+            Vector2 shakePos = Random.insideUnitCircle * shakeAmout;
+            transform.position = new Vector3(transform.position.x + shakePos.x, transform.position.y + shakePos.y, transform.position.z);
+            shakeTimer -= Time.deltaTime;
+        }
+    }
+
+    public void ShakeCamera(float shakePwr, float shakeDur)
+    {
+        shakeAmout = shakePwr;
+        shakeTimer = shakeDur;
+    }
+}
