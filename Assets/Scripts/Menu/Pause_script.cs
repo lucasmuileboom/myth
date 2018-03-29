f2d6db7966f3fca47dd4cb82287bfe85dcc4c591
blizzard190 <22256@ma-web.nl>
2018-03-29 13:42:36 +0200

    Pause
    Pause menu call script added and made

---
+++ Assets/Scripts/Menu/Pause_script.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_script : MonoBehaviour {
+
+    GameObject[] pauseObjects;
+
+	void Start () {
+        Time.timeScale = 1;
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPaused");
+        hidePause();
+	}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PauseControl();
+        }
+    }
+
+    public void PauseControl()
+    {
+        if (Time.timeScale > 0)
+        {
+            Time.timeScale = 0;
+            showPaused();
+        }
+        else if (Time.timeScale == 0)
+        {
+           Time.timeScale = 1;
+           hidePause();
+        }
+    }
+
+    public void showPaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    public void hidePause()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+}
