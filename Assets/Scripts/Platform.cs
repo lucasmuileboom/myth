5c9acf69fc23389b6f3733d2edcf40d1af427f5f
lucasmuileboom <muileboomlucas@gmail.com>
2018-04-09 12:38:50 +0200

    player fix, platform art

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -6,20 +6,20 @@ public class Platform : MonoBehaviour
 {
     private InputManager _inputManager;
     private GameObject _Player;
-    private float _playerHeight;
-    private float _platformHeight;
+   // private float _playerHeight;
+   [SerializeField]private float _platformHeight;
     private bool _goDown;
 
     void Start ()
     {
         _Player = GameObject.Find("Player");
         _inputManager = _Player.GetComponent<InputManager>();
-        _playerHeight = _Player.transform.lossyScale.y / 2;
-        _platformHeight = transform.lossyScale.y / 2;
+        //_playerHeight = _Player.transform.lossyScale.y / 2;
+        //_platformHeight = transform.lossyScale.y / 2;
     }
 	void Update ()
     {
-        if (_Player.transform.position.y - _playerHeight > transform.position.y + _platformHeight && !_goDown)
+        if (_Player.transform.position.y > transform.position.y + _platformHeight && !_goDown)
         {
             if (_inputManager.down())
             {
@@ -32,7 +32,7 @@ public class Platform : MonoBehaviour
                 gameObject.layer = 8;
             }
         }
-        else if (_Player.transform.position.y - _playerHeight < transform.position.y)
+        else if (_Player.transform.position.y < transform.position.y)
         {
             _goDown = false;
             GetComponent<BoxCollider2D>().enabled = false;
--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -8,6 +8,7 @@ public class PlayerAttack : MonoBehaviour
     private PlayerMovement _PlayerMovement;
     [SerializeField]private LayerMask _enemysLayer;
     [SerializeField]private GameObject _projectile;
+    [SerializeField] private float _yOffset;
     [SerializeField]private float _minProjectileSpeed;
     [SerializeField]private float _maxProjectileSpeed;
     [SerializeField]private float _maxProjectileDamage;
@@ -21,6 +22,7 @@ public class PlayerAttack : MonoBehaviour
         public int Damage;
     }
     private Vector2 direction;
+    private Vector3 _offset;
     private float _timer = 0;
     private float _projectileSpeed;
     private float _projectileDamage;
@@ -30,6 +32,7 @@ public class PlayerAttack : MonoBehaviour
     {
         _inputManager = GetComponent<InputManager>();
         _PlayerMovement = GetComponent<PlayerMovement>();
+        _offset = new Vector3(0, _yOffset,0);
     }
     void Update()
     {
@@ -62,8 +65,8 @@ public class PlayerAttack : MonoBehaviour
             }
             if (melee)
             {
-                Debug.DrawRay(transform.position, direction * Attacks[i].Range, Color.red);
-                RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, Attacks[i].Range, _enemysLayer);
+                Debug.DrawRay(transform.position + _offset, direction * Attacks[i].Range, Color.red);
+                RaycastHit2D hit = Physics2D.Raycast(transform.position + _offset, direction, Attacks[i].Range, _enemysLayer);
                 if (hit.collider != null)
                 {
                     hit.collider.gameObject.GetComponent<EnemyHealth>().TakeDamage(Attacks[i].Damage);
@@ -86,7 +89,6 @@ public class PlayerAttack : MonoBehaviour
     }
     private IEnumerator HoldRangeAttack()
     {
-        print(_chargeAttack);
         while (_inputManager.holdRangeAttack() && _projectileSpeed < _maxProjectileSpeed && _projectileDamage < _maxProjectileDamage)
         {
             _projectileDamage += (_maxProjectileDamage - Attacks[2].Damage) / _chargeSpeed;
@@ -105,12 +107,10 @@ public class PlayerAttack : MonoBehaviour
     }
     private void RangeAttack()
     {
-        print(_projectileSpeed);
-        GameObject projectile = Instantiate(_projectile, new Vector2(transform.position.x, transform.position.y) + direction, Quaternion.Euler(0, 0, 0)) as GameObject;
+        GameObject projectile = Instantiate(_projectile, new Vector2(transform.position.x, transform.position.y + _yOffset) + direction, Quaternion.Euler(0, 0, 0)) as GameObject;
         projectile.GetComponent<Rigidbody2D>().velocity = direction * _projectileSpeed;
         projectile.GetComponent<Projectile>().damage = _projectileDamage;
         _chargeAttack = false;
         _timer = Attacks[2].FireRate;
-        print(_chargeAttack);
     }
 }
