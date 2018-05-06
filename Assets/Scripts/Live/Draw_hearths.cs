f1595943408c658d0fd3c59d2699ab5b9373f789
blizzard190 <22256@ma-web.nl>
2018-05-06 11:48:41 +0200

    Live script

---
+++ Assets/Scripts/Live/Draw_hearths.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw_hearths : MonoBehaviour {
+
+    private Transform[] _Lives;
+    private int _lastIndex;
+
+    void Start() {
+        _Lives = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            _Lives[i] = transform.GetChild(i);
+        }
+        _lastIndex = _Lives.Length - 1;
+    }
+
+    void AwakeHearth()
+    {
+        _lastIndex++;
+        _Lives[_lastIndex].gameObject.SetActive(true);
+    }
+
+    void HideHearth()
+    {
+        _Lives[_lastIndex].gameObject.SetActive(false);
+        _lastIndex--;
+    }
+}
